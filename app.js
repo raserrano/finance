886297854f9fdbc5b70a6e48ce6f12ad1842f776
Raserrano <betoseru23@gmail.com>
2017-01-20 18:09:10 -0600

    Adding more features

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/currencies', currencies);
+app.use('/cdps', cdps);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
