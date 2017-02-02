3c85b4c7ff5ad783ece4a7470bbd6b9749e6b404
Raserrano <betoseru23@gmail.com>
2017-02-02 15:27:46 -0600

    Adding hound suggestions

--- routes/interests.js
+++ routes/interests.js
@@ -4,7 +4,7 @@ var express = require('express'),
     bodyParser = require('body-parser'), //parses information from POST
     methodOverride = require('method-override'); //used to manipulate POST
 
-router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 router.use(methodOverride(function(req, res){
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
@@ -12,7 +12,7 @@ router.use(methodOverride(function(req, res){
     delete req.body._method;
     return method;
   }
-}))
+}));
 
 router.route('/')
   .get(function(req, res, next) {
