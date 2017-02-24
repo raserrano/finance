197abe49c5cb35dfea594a1ced8f9cf05ed557d7
Raserrano <betoseru23@gmail.com>
2017-02-24 13:22:59 -0600

    Applying hound suggestions

--- routes/currencies.js
+++ routes/currencies.js
@@ -7,10 +7,10 @@ var express = require('express'),
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(methodOverride(function(req, res){
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
+    // Look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
   }
 }));
 
